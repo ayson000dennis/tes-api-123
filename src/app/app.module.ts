9be039d1afed1a468b4040bbf2d86c3abdb5c921
ricson <ricson@fullstack.ph>
2017-09-25 20:15:41 +0800

    data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,78 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, ErrorHandler } from '@angular/core';
-import { HttpModule } from '@angular/http';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
-import { Facebook } from '@ionic-native/facebook';
-import { GooglePlus } from '@ionic-native/google-plus';
-
-import { SliderPage } from '../pages/page-slider/page-slider';
-import { LoginPage } from '../pages/page-login/page-login';
-import { ResetPassPage } from '../pages/page-reset-pass/page-reset-pass';
-import { ResetPassSuccessPage } from '../pages/page-reset-pass-success/page-reset-pass-success';
-import { SignupPage } from '../pages/page-signup/page-signup';
-import { SignupSuccessPage } from '../pages/page-signup-success/page-signup-success';
-import { ItemDetailsPage } from '../pages/item-details/item-details';
-import { UserLoyaltyPage } from '../pages/page-user-loyalty/page-user-loyalty';
-import { UserFavoritesPage } from '../pages/page-user-favorites/page-user-favorites';
-import { UserDealsPage } from '../pages/page-user-deals/page-user-deals';
-import { UserDealsDetailsPage } from '../pages/page-user-deals-details/page-user-deals-details';
-import { UserCardPage } from '../pages/page-user-card/page-user-card';
-import { ListPage } from '../pages/list/list';
-
-import { StatusBar } from '@ionic-native/status-bar';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { ScreenOrientation } from '@ionic-native/screen-orientation';
-
-import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
-
+import { StatusBar } from '@ionic-native/status-bar';
+ 
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+ 
+import { NgxQRCodeModule } from 'ngx-qrcode2';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+ 
 @NgModule({
   declarations: [
     MyApp,
-    SliderPage,
-    LoginPage,
-    ResetPassPage,
-    ResetPassSuccessPage,
-    SignupPage,
-    SignupSuccessPage,
-    ItemDetailsPage,
-    UserLoyaltyPage,
-    UserFavoritesPage,
-    UserDealsPage,
-    UserDealsDetailsPage,
-    UserCardPage,
-    ListPage
+    HomePage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    HttpModule
+    NgxQRCodeModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    SliderPage,
-    LoginPage,
-    ResetPassPage,
-    ResetPassSuccessPage,
-    SignupPage,
-    SignupSuccessPage,
-    ItemDetailsPage,
-    UserLoyaltyPage,
-    UserFavoritesPage,
-    UserDealsPage,
-    UserDealsDetailsPage,
-    UserCardPage,
-    ListPage
+    HomePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    QRScanner,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    ScreenOrientation,
-    Facebook,
-    GooglePlus
+    BarcodeScanner
   ]
 })
 export class AppModule {}
--- src/app/config.ts
+++
@@ -1,5 +0,0 @@
-const config = {
-  baseUrl : 'https://warm-beach-10670.herokuapp.com/'
-  // baseUrl : 'http://localhost:5012/'
-}
-export default config;
