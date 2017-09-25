56f6633c2db3d717598dc935c784ca0102208125
ricson <ricson@fullstack.ph>
2017-09-25 21:16:11 +0800

    Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,59 +1,22 @@
-import { Component, ViewChild } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
-import { Platform, MenuController, Nav } from 'ionic-angular';
-
-import { SliderPage } from '../pages/page-slider/page-slider';
-import { UserLoyaltyPage } from '../pages/page-user-loyalty/page-user-loyalty';
-
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { ScreenOrientation } from '@ionic-native/screen-orientation';
 
+import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
-
 export class MyApp {
-  @ViewChild('nav') nav: NavController;
+  rootPage:any = HomePage;
 
-  // make SliderPage the root (or first) page
-  // public rootPage: any = SliderPage;
-  public rootPage: any = UserLoyaltyPage;
-  pages: Array<{title: string, component: any}>;
-
-  constructor(
-    public platform: Platform,
-    public menu: MenuController,
-    public statusBar: StatusBar,
-    public splashScreen: SplashScreen,
-    public screenOrientation: ScreenOrientation
-  ) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
-      this.screenOrientation.lock(screenOrientation.ORIENTATIONS.PORTRAIT);
-    });
-
-    this.initializeApp();
-
-    // set our app's pages
-    this.pages = [
-      { title: 'LOYALTY', component: UserLoyaltyPage }
-    ];
-  }
-
-  initializeApp() {
-    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      statusBar.styleDefault();
+      splashScreen.hide();
     });
   }
-
-  openPage(page) {
-    // close the menu when clicking a link from the menu
-    this.menu.close();
-    // navigate to the new page if it is not the current page
-    this.nav.setRoot(page.component);
-  }
 }
+
